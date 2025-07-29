dd9624991b9cd3403ce380ccda9c5c6ee6e707c7
JeanBreves98 <JeanBreves1998@gmail.com>
2025-07-29 13:00:39 -0300

    Initial commit

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/example/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.example.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component // Marks the class as a Spring bean
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
