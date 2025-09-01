8c91af29ae743fcf0a78ed701e02acd1f3b17b1c
jean <jeanbreves1998@gmail.com>
2025-09-01 14:48:49 -0300

    01/09/2025 - 14:48

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/thymeleafdemo/controller/DemoController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/thymeleafdemo/controller/DemoController.java
@@ -12,7 +12,7 @@ public class DemoController {
     @GetMapping("/hello")
     public String sayHello(Model theModel) {
 
-        theModel.addAttribute("theDate", java.time.LocalDate.now());
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
 
         return "helloworld";
     }
