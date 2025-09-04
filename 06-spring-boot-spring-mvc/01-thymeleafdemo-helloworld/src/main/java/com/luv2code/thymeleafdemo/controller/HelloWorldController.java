de17974fccb6151230624c3a83e861984f74bfb5
jean <jeanbreves1998@gmail.com>
2025-09-04 15:27:38 -0300

    04/09/2025 - 15:27

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/thymeleafdemo/controller/HelloWorldController.java
@@ -2,14 +2,17 @@ package com.luv2code.thymeleafdemo.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     // need a controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
     }
@@ -34,6 +37,21 @@ public class HelloWorldController {
 
         // create the message;
         String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all uppercase
+        theName = theName.toUpperCase();
+
+        // create the message;
+        String result = "Hey my friend " + theName;
 
         // add message to the model
         model.addAttribute("message", result);
