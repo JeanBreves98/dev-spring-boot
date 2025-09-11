068a6517b5c194811393b6a670fa08ed81760b48
jean <jeanbreves1998@gmail.com>
2025-09-11 15:38:36 -0300

    11/09/2025 - 15:38

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,18 @@ public class Customer {
     @NotNull(message = "Last name is required")
     @Size(min = 1, message = "Last name must have at least 1 character")
     private String lastName;
+
+    @Min(value = 0, message = "Must be greater than or equal to 0")
+    @Max(value = 10, message = "Must be less than or equal to 10")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
